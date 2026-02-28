985977ae45b2c822c195c32c432f5c1e34d09bc6
Muneer Ahmad <munirjhangir786@gmail.com>
2026-02-28 10:48:33 +0500

    Create Pattern 7.cpp

---
+++ Pattern 7.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main() {
+     for(int row=1;row<=4;row++)
+    {
+        for(int col=1;col<=row;col++)
+        {
+           cout<<"* ";
+        }
+        for(int col=1;col<=4-row;col++)
+        {
+           cout<<"  ";
+        }
+        
+          for(int col=1;col<=4-row;col++)
+        {
+           cout<<"  ";
+        }
+        for(int col=1;col<=row;col++)
+        {
+           cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int row=1;row<=3;row++)
+    {
+        for(int col=3;col>=row;col--)
+        {
+           cout<<"* ";
+        }
+          for(int col=1;col<=row;col++)
+        {
+           cout<<"  ";
+        }
+        
+             for(int col=1;col<=row;col++)
+        {
+           cout<<"  ";
+        }
+        
+        for(int col=3;col>=row;col--)
+        {
+           cout<<"* ";
+        }
+        cout<<endl;
+    }
+     
+
+    return 0;
+}

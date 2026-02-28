14f032d245a562040c5b209818204493aa8a5fd9
Muneer Ahmad <munirjhangir786@gmail.com>
2026-02-28 13:36:44 +0500

    Create Do While Number print.cpp

---
+++ Do While Number print.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"Enter the number to print from 1 to that number  ";
+   cin>>n;
+   int i=1;
+ 
+  do
+{
+      cout<<i<<endl;
+      i++;
+}while(i<=n);
+    return 0;
+}

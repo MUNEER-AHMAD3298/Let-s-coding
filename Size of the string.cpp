e032bf7d745f6d498a9e4d38174f9c7b04e40537
Muneer Ahmad <munirjhangir786@gmail.com>
2026-03-11 21:32:37 +0500

    Create Size of the string.cpp

---
+++ Size of the string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+ // Size of the string
+ string s;
+ cout<<"Enter any String  ";
+ getline(cin,s);
+ cout<<s.size();
+ 
+
+    return 0;
+}

351fd92773ddfc9929d9a9024dd829d7f729cdc4
Muneer Ahmad <munirjhangir786@gmail.com>
2026-03-11 21:22:06 +0500

    Create last.digit.remove of the string.cpp

---
+++ last.digit.remove of the string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+ string s;
+ //remove last digit of string
+ cout<<"Enter any string  ";
+ cin>>s;
+ s.pop_back();
+ cout<<s;
+ 
+
+    return 0;
+}
